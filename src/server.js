f57eddd6fb025207ecf05eeda30fe21c993d095e
pranjalsawant-cointab <pranjal.sawant@cointab.net>
2025-12-22 11:26:49 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,9 +6,13 @@ app.use(express.json())
 let users = []
 
 app.post("/user", (req, res) => {
-  users.push(req.body)
-  res.send("User added")
-})
+  const user = req.body;
+  if (!user || typeof user.name !== 'string' || user.name.trim() === '') {
+    return res.status(400).json({ error: "Invalid user data" });
+  }
+  users.push(user);
+  res.send("User added");
+});
 
 app.get("/user/:id", (req, res) => {
   const user = users[req.params.id]
